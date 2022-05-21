9d5ea644f9c5b34a78a06ad0bd6780fb692b9e29
Sho Iwamoto <contact@misho-web.com>
2022-05-21 20:57:28 +0200

    enhance Validator to allow automatic PDF output

--- SimpleLHCBoundValidator.m
+++ SimpleLHCBoundValidator.m
@@ -1,6 +1,6 @@
 (* ::Package:: *)
 
-(* Time-Stamp: <2022-04-09 18:20:09> *)
+(* Time-Stamp: <2022-05-22 20:38:07> *)
 
 (* :Title: Validation Helper for Simple LHC Bound *)
 (* :Context: SimpleLHCBoundValidator` *)
@@ -33,6 +33,10 @@ GetFrame::usage = FMT["
 Debugging information is stored in `DebugInfo`.
 "];
 
+ImageClear::usage = FMT["
+*ImageClear*[#image#, #left#, #right#, #bottom#, #top#] clears specified pixels of #image#.
+"];
+
 FindOverlay::usage = FMT["
 *FindOverlay*[#plot#, #image#, #frame#, #imageX#, #imageY#] tries to find the parameter set for `PlotOverlay` function.
 #plot# is the plot to be validated, #image# is to be the actual LHC figure.
@@ -53,12 +57,13 @@ DefaultSize = 400;
 
 Begin["`Private`"];
 Unprotect[GetFrame, FindOverlay, PlotOverlay];
+$OutputPDF = False;
 
 GetFrame[image_] := Block[{edge, lines, horizontal, vertical, x, y, w, h},
-  edge = EdgeDetect[Binarize[image]];
-  lines = ImageLines[edge, 0.2, MaxFeatures->4];
-  horizontal = Select[lines, -0.05 < Mod[(ArcTan@@(#[[1,1]]-#[[1,2]]))/Pi, 1, -0.5] < 0.05&];
-  vertical   = Select[lines,  0.45 < Mod[(ArcTan@@(#[[1,1]]-#[[1,2]]))/Pi, 1      ] < 0.55&];
+  edge = EdgeDetect[Binarize[RemoveAlphaChannel[image]]];
+  lines = ImageLines[edge, 0.2, MaxFeatures->8];
+  horizontal = Select[lines, -0.05 < Mod[(ArcTan@@(#[[1,1]]-#[[1,2]]))/Pi, 1, -0.5] < 0.05&][[;;2]];
+  vertical   = Select[lines,  0.45 < Mod[(ArcTan@@(#[[1,1]]-#[[1,2]]))/Pi, 1      ] < 0.55&][[;;2]];
   w = Max[#[[1, All, 1]] &/@ horizontal];
   h = Max[#[[1, All, 2]] &/@ vertical];
   x = Mean[#[[1, All, 1]]] &/@ vertical;
@@ -70,14 +75,24 @@ GetFrame[image_] := Block[{edge, lines, horizontal, vertical, x, y, w, h},
   <|"Size"->{w, h}, "X"->Sort[x], "Y"->Sort[y]|>
 ];
 
+
+ImageClear[image_, left_:0, right_:0, bottom_:0, top_:0] := Block[{dim = ImageDimensions[image]},
+  image
+  // ImageTrim[#, {{0 + left, 0 + bottom}, {dim[[1]] - right, dim[[2]] - top}}] &
+  // ImagePad[#, {{left, right}, {bottom, top}}, White] &
+]
+
 d[{x_, y_}] := Abs[y - x];
 
-FindOverlay[plot_, image_, frame_, imageX_, imageY_] := Module[{
-  plotX, plotY, rx, ry, newX, newY, newSize, aspect
+FindOverlay[plot_, image_, framein_:None, imageXin_:None, imageYin_:None] := Block[{
+  plotX, plotY, frame, imageX, imageY, rx, ry, newX, newY, newSize, aspect, result
 },
+  {plotX, plotY} = PlotRange /. plot[[2]];
+  frame  = If[framein === None, GetFrame[image], framein];
+  imageX = If[imageXin === None, plotX, imageXin];
+  imageY = If[imageYin === None, plotY, imageYin];
   If[{Length[frame["X"]], Length[frame["Y"]]} != {2, 2}, Message[FindOverlay::invalid]; Abort[]];
 
-  {plotX, plotY} = PlotRange /. plot[[2]];
   rx = DefaultSize / frame["Size"][[1]];
   ry = rx * d[frame["X"]] / d[frame["Y"]] * d[plotX] / d[plotY] * d[imageY] / d[imageX];
   newX = rx * frame["X"] // Round[#, 0.01]&;
@@ -88,16 +103,28 @@ FindOverlay[plot_, image_, frame_, imageX_, imageY_] := Module[{
     Line[{{#, 0}, {#, newSize[[2]]}}] &/@ newX
   ]];
   aspect = d[imageX] * newSize[[1]] / d[newX] // Round[#, 0.1]&;
-  {newSize, {imageX[[1]], imageY[[1]]}, {newX[[1]], newY[[1]]}, aspect}
+  result = {newSize, {imageX[[1]], imageY[[1]]}, {newX[[1]], newY[[1]]}, aspect};
+  Print[InputForm[TextString[result]]];
+  result
 ]
 
+Attributes[PlotOverlay] = {HoldFirst};
 Options[PlotOverlay] = {"alpha" -> 0.5};
-PlotOverlay[plot_, image_, parameters_, OptionsPattern[]] := Show[plot, Prolog->Inset[
+PlotOverlay[plot_, image_, parameters_, OptionsPattern[]] := Block[{title, output, filename},
+  title = Cases[Hold[plot], _Symbol[n_String] :> n, 1];
+  output = Show[plot, Prolog->Inset[
     ImageResize[SetAlphaChannel[image, OptionValue["alpha"]], parameters[[1]]],
     parameters[[2]],
     parameters[[3]],
     parameters[[4]]
-], PlotRangeClipping->False];
+  ], PlotRangeClipping->False];
+  If[$OutputPDF && Length[title] > 0,
+    filename = "validation." <> StringReplace[title[[1]], RegularExpression["\\W"]->""] <> ".pdf";
+    If[MemberQ[$ContextPath, "PlotTools`"], output = PlotTools`EvaluatePlot[output]];
+    Export[filename, Magnify[output, 1]];
+  ];
+  output];
+
 
 End[];
 Protect[GetFrame, FindOverlay, PlotOverlay];
